b92fce3a2993918ae8c31bdbb17396af4442fa58
Erlend Røsjø <erlend@erlend.net>
2024-10-26 13:40:54 +0200

    Rum scraping

--- VmpScraper/Ildvann.VmpScraper/Ildvann.VmpScraper/VmpScraperService.cs
+++ VmpScraper/Ildvann.VmpScraper/Ildvann.VmpScraper/VmpScraperService.cs
@@ -1,21 +1,52 @@
-using System.Text.Json;
 using Microsoft.Extensions.Logging;
 using Vinmonopolet;
+using Vinmonopolet.Models;
+using Vinmonopolet.Models.Facets.MainCategory;
+using Vinmonopolet.Models.Facets.MainSubCategory;
 using Vinmonopolet.QueryBuilder;
 
 namespace Ildvann.VmpScraper;
 
-public class VmpScraperService(ILogger<VmpScraperService> logger, IVinmonopoletClient vinmonopoletClient)
+public class VmpScraperService(ILogger<VmpScraperService> logger, IVinmonopoletClient vmpClient)
 {
     private readonly ILogger<VmpScraperService> _logger = logger;
-    private readonly IVinmonopoletClient _vinmonopoletClient = vinmonopoletClient;
 
     public async Task Run()
     {
-        var query = new ProductQueryBuilder();
-        var res =  await _vinmonopoletClient.GetProducts(query.Build());
-        Console.WriteLine("Got {0} products", res.Products.Count);
+        var query = new ProductQueryBuilder()
+            .MainCategory(MainCategoryFacets.Liquor)
+            .MainSubCategory(MainSubCategoryFacets.Rum);
+
+        var productQueryBuilderResult = query.Build();
         
-        await OutputWriter.WriteBaseProductsToJsonAsync(res.Products, "products.json");
+        var res =  await vmpClient.GetProducts(productQueryBuilderResult, new PaginationOptions
+        {
+            CurrentPage = 0,
+            PageSize = 50
+        });
+        LogResult(res);
+        
+        List<BaseProduct> baseProducts = [];
+        baseProducts.AddRange(res.Products);
+        
+        while (res.Pagination.CurrentPage <= res.Pagination.TotalPages)
+        {
+            res = await res.NextPage(vmpClient);
+            LogResult(res);
+            baseProducts.AddRange(res.Products);
+            Console.WriteLine("Added {0} products", res.Products.Count);
+        }     
+        
+        Console.WriteLine("Total products: {0}", baseProducts.Count);
+        await OutputWriter.WriteBaseProductsToJsonAsync(baseProducts, "products.json");
+    }
+
+    private static void LogResult(GetProductsResponse res)
+    {
+        Console.WriteLine("Total results: {0}", res.Pagination.TotalResults);
+        Console.WriteLine("Page size: {0}", res.Pagination.PageSize);
+        Console.WriteLine("Current page: {0}", res.Pagination.CurrentPage);
+        Console.WriteLine("Total pages: {0}", res.Pagination.TotalPages);
+        Console.WriteLine("Current query: '{0}'", res.CurrentQuery.Query.Value);
     }
 }
